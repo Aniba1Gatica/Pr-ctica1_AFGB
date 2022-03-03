91f0129070f34728ba9f8f467bbb547be2459579
Anibal <anibal.gatica1701@alumnos.ubiobio.cl>
2022-03-03 19:54:45 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Práctica1_AFGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Start_Camera = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guardar = new System.Windows.Forms.Button();
+            this.temporizador = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Reiniciar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(572, 50);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Video en vivo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(31, 456);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(200, 237);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Start_Camera
+            // 
+            this.Start_Camera.BackColor = System.Drawing.Color.Red;
+            this.Start_Camera.Location = new System.Drawing.Point(244, 456);
+            this.Start_Camera.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Start_Camera.Name = "Start_Camera";
+            this.Start_Camera.Size = new System.Drawing.Size(122, 37);
+            this.Start_Camera.TabIndex = 12;
+            this.Start_Camera.Text = "Iniciar cámara";
+            this.Start_Camera.UseVisualStyleBackColor = false;
+            this.Start_Camera.Click += new System.EventHandler(this.Start_Camera_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 83);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(573, 319);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guardar
+            // 
+            this.guardar.BackColor = System.Drawing.Color.Lime;
+            this.guardar.Location = new System.Drawing.Point(244, 642);
+            this.guardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(122, 37);
+            this.guardar.TabIndex = 16;
+            this.guardar.Text = "Guardar datos";
+            this.guardar.UseVisualStyleBackColor = false;
+            this.guardar.Click += new System.EventHandler(this.guardar_Click);
+            // 
+            // temporizador
+            // 
+            this.temporizador.Interval = 5000;
+            this.temporizador.Tick += new System.EventHandler(this.temporizador_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(31, 411);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 50);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Registros datos";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(640, 83);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(717, 319);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.Orange;
+            this.Stop.Location = new System.Drawing.Point(244, 517);
+            this.Stop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(122, 37);
+            this.Stop.TabIndex = 19;
+            this.Stop.Text = "Detener cámara";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Reiniciar
+            // 
+            this.Reiniciar.BackColor = System.Drawing.Color.Aqua;
+            this.Reiniciar.Location = new System.Drawing.Point(244, 579);
+            this.Reiniciar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Reiniciar.Name = "Reiniciar";
+            this.Reiniciar.Size = new System.Drawing.Size(122, 37);
+            this.Reiniciar.TabIndex = 20;
+            this.Reiniciar.Text = "Reiniciar cámara";
+            this.Reiniciar.UseVisualStyleBackColor = false;
+            this.Reiniciar.Click += new System.EventHandler(this.Reiniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.Reiniciar);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Start_Camera);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Start_Camera;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.Timer temporizador;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Reiniciar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Práctica1_AFGB
+{
+    public partial class Form1 : Form
+    {
+        Mat m = new Mat();
+        VideoCapture captura = null;
+        IBackgroundSubtractor backgroundSubtractor;
+        int minArea = 7000;//El área minima a tomar en consideración para hacer el rectángulo al rededor del objeto o figura
+        string fechas;
+        string hora;
+        int contador = 1;
+        bool Pausa = false;
+        int X = 0;//Coordenada X
+        int Y = 0;//Coordenada Y
+        int dif = 0; //Variable que almacena la diferencia de posiciones en X
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Camera_Click(object sender, EventArgs e)//Acción del botón Iniciar cámara
+        {
+            if (captura == null)
+            {
+                captura = new Emgu.CV.VideoCapture(0);//Crea la instancia captura en caso que sea null
+
+            }
+            Start_Camera.Text = "Iniciar Cámara";
+            Start_Camera.BackColor = Color.Lime;
+            captura.ImageGrabbed += Captura_ImageGrabbed;//Logra tomar la imagen a través de la función que esta más abajo
+            captura.Start();
+            temporizador.Start();
+            capturaImagen();
+        }
+
+        private void Captura_ImageGrabbed(object sender, EventArgs e)
+        {
+            if (captura != null)
+            {
+                try
+                {
+                    captura.Retrieve(m);//Toma la captura
+                    pictureBox1.Image = m.ToBitmap();//La muestra en la caja de imagen "Video en vivo"
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void guardar_Click(object sender, EventArgs e)//Acción del botón Guardar
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(saveFileDialog1.FileName + " .txt", richTextBox1.Lines);//Crea un archivo .txt con los datos de la caja de texto
+            }
+        }
+
+        private void temporizador_Tick(object sender, EventArgs e)
+        {
+            hora = DateTime.Now.ToString("HH:mm:ss");
+            fechas = DateTime.Now.ToString("dddd/MMM/yyyy");
+
+            dif = X - dif;//Cálculo para ver la diferencia de posiciones en X
+            if (dif < 0)
+            {
+                dif *= (-1);//Cálculo para convertir la diferencia en un valor positivo en caso que sea negativo
+            }
+
+            if (fechas.Length > 0 && hora.Length > 0)
+            {
+                richTextBox1.Text += ("Captura #") + contador + "\n";
+                richTextBox1.Text += ("Fecha: ") + fechas + "\n";
+                richTextBox1.Text += ("Hora: ") + hora + "\n";
+                richTextBox1.Text += ("Posición X: ") + X + "\n";
+                richTextBox1.Text += ("Posición Y: ") + Y + "\n";
+                richTextBox1.Text += ("Diferencia de posiciones en X: ") + dif + "\n";
+                richTextBox1.Text += ("-------------------------------------------------------- \n");
+                contador++;
+            }
+        }
+
+        //Funciones
+
+        public void capturaImagen()//Función para capturar y procesar las imágenes de la cámara
+        {
+            backgroundSubtractor = new BackgroundSubtractorMOG2();
+            //backgroundSubtractor = new Emgu.CV.BgSegm.BackgroundSubtractorCNT();
+            Application.Idle += Application_Idle;
+
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            if (captura != null)
+            {
+                
+                Pausa = !Pausa;
+                if (!Pausa)
+                {
+                    try
+                    {
+                        Image<Gray, byte> imgOutput = m.ToImage<Gray, byte>().ThresholdBinary(new Gray(100), new Gray(255));
+                        Mat frame = captura.QueryFrame();
+                        if (frame.IsEmpty)
+                        {
+                            Application.Idle -= Application_Idle;
+                            return;
+                        }
+
+
+
+                        Mat framessuaves = new Mat();
+                        CvInvoke.GaussianBlur(imgOutput, framessuaves, new Size(3, 3), 1);//Suaviza los frames
+
+                        Mat fondoExtraccion = new Mat();
+                        backgroundSubtractor.Apply(framessuaves, fondoExtraccion);//Se aplica la extracción del fondo de la imagen
+
+                        //Proceso para limpiar el fondo de frames residuales
+                        CvInvoke.Threshold(framessuaves, framessuaves, 200, 240, ThresholdType.Binary);
+                        CvInvoke.MorphologyEx(framessuaves, framessuaves,
+                            MorphOp.Close, Mat.Ones(7, 3, DepthType.Cv8U, 1), new Point(-1, -1), 1, BorderType.Reflect, new Emgu.CV.Structure.MCvScalar(0));
+
+                        //Detecta los contornos de los objetos o figuras en la imagen
+                        VectorOfVectorOfPoint contornos = new VectorOfVectorOfPoint();
+                        CvInvoke.FindContourTree(framessuaves, contornos, ChainApproxMethod.ChainApproxSimple, new Point(-1, -1));
+
+                        for (int i = 0; i < contornos.Size; i++)
+                        {
+                            var bbox = CvInvoke.BoundingRectangle(contornos[i]);
+                            var area = bbox.Width * bbox.Height;
+                            var ar = (float)bbox.Height / bbox.Width;
+
+                            if (area > minArea && ar < 1.0)
+                            {
+                                CvInvoke.Rectangle(frame, bbox, new Emgu.CV.Structure.MCvScalar(0, 0, 255), 2);
+                                X = bbox.Width;
+                                Y = bbox.Height;
+                            }
+                        }
+
+
+                        pictureBox2.Image = frame.ToBitmap();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    
+                    Pausa = !Pausa;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            captura = null;
+            temporizador.Stop();
+        }
+
+        private void Reiniciar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            temporizador.Stop();
+            temporizador.Start();
+        }
+    }
+}
